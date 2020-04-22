bce96a8326232e871fc4f26499579c8a78bbd75e
Lee Cheneler <lacheneler@gmail.com>
2020-04-22 11:56:44 +0100

    added @babel/plugin-proposal-nullish-coalescing-operator to babel… (#16)

--- packages/babel-preset/index.js
+++ packages/babel-preset/index.js
@@ -21,6 +21,7 @@ module.exports = () => {
       "@babel/plugin-proposal-class-properties",
       "@babel/plugin-proposal-object-rest-spread",
       "@babel/plugin-proposal-optional-chaining",
+      "@babel/plugin-proposal-nullish-coalescing-operator",
       ["@babel/plugin-transform-runtime", { regenerator: true }],
     ],
   };
