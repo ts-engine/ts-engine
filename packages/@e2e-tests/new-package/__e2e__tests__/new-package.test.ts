00b07fc8d07df1dc7f0089e8271e12c6a70c341e
leecheneler <lacheneler@gmail.com>
2020-05-14 16:14:45 +0100

    GH-9 added new=package command

---
+++ packages/@e2e-tests/new-package/__e2e__tests__/new-package.test.ts
@@ -0,0 +1,212 @@
+import { fileSystem, runCliCommand } from "@helpers/test-utils";
+import packageJson from "../package.json";
+
+describe("new-package", () => {
+  beforeEach(async () => {
+    await fileSystem.deleteDir("new-package");
+  });
+
+  it("should create a new node app package", async () => {
+    const runner = runCliCommand(
+      "yarn run ts-engine new-package --node-app --license MIT --name new-package"
+    );
+
+    // Wait for tool to complete
+    const statusCode = await runner.waitForStatusCode();
+
+    // Should exit successfully
+    expect(statusCode).toBe(0);
+
+    // Built file is written to file system
+    expect(await fileSystem.fileExists("new-package/package.json")).toBe(true);
+    expect(await fileSystem.fileExists("new-package/src/main.ts")).toBe(true);
+
+    // Package filled in correctly
+    expect(
+      JSON.parse(await fileSystem.readFile("new-package/package.json"))
+    ).toMatchObject({
+      name: "new-package",
+      version: "1.0.0",
+      license: "MIT",
+      private: true,
+      scripts: {
+        build: "ts-engine build --node-app",
+        lint: "ts-engine lint",
+        test: "ts-engine test",
+        typecheck: "ts-engine typecheck",
+      },
+      devDependencies: {
+        "@ts-engine/cli": `^${packageJson.version}`,
+      },
+    });
+
+    // source code correct
+    expect(await fileSystem.readFile("new-package/src/main.ts")).toBe(
+      'console.log("hello world");'
+    );
+  });
+
+  it("should create a new library package", async () => {
+    const runner = runCliCommand(
+      "yarn run ts-engine new-package --library --license MIT --name new-package"
+    );
+
+    // Wait for tool to complete
+    const statusCode = await runner.waitForStatusCode();
+
+    // Should exit successfully
+    expect(statusCode).toBe(0);
+
+    // New package files are present
+    expect(await fileSystem.fileExists("new-package/package.json")).toBe(true);
+    expect(await fileSystem.fileExists("new-package/src/main.ts")).toBe(true);
+
+    // Package filled in correctly
+    expect(
+      JSON.parse(await fileSystem.readFile("new-package/package.json"))
+    ).toMatchObject({
+      name: "new-package",
+      version: "1.0.0",
+      license: "MIT",
+      private: false,
+      scripts: {
+        build: "ts-engine build --library",
+        lint: "ts-engine lint",
+        test: "ts-engine test",
+        typecheck: "ts-engine typecheck --emit",
+      },
+      devDependencies: {
+        "@ts-engine/cli": `^${packageJson.version}`,
+      },
+    });
+
+    // source code correct
+    expect(await fileSystem.readFile("new-package/src/main.ts")).toBe(
+      'export const printHelloWorld = () => console.log("hello world");'
+    );
+  });
+
+  it("should default license if not provided", async () => {
+    const runner = runCliCommand(
+      "yarn run ts-engine new-package --library --name new-package"
+    );
+
+    // Wait for tool to complete
+    const statusCode = await runner.waitForStatusCode();
+
+    // Should exit successfully
+    expect(statusCode).toBe(0);
+
+    // New package files are present
+    expect(await fileSystem.fileExists("new-package/package.json")).toBe(true);
+    expect(await fileSystem.fileExists("new-package/src/main.ts")).toBe(true);
+
+    // Package filled in correctly
+    expect(
+      JSON.parse(await fileSystem.readFile("new-package/package.json"))
+    ).toMatchObject({
+      name: "new-package",
+      version: "1.0.0",
+      license: "UNLICENSED",
+      private: false,
+      scripts: {
+        build: "ts-engine build --library",
+        lint: "ts-engine lint",
+        test: "ts-engine test",
+        typecheck: "ts-engine typecheck --emit",
+      },
+      devDependencies: {
+        "@ts-engine/cli": `^${packageJson.version}`,
+      },
+    });
+  });
+
+  it("should enforce --node-app or --library to be specified", async () => {
+    const runner = runCliCommand(
+      "yarn run ts-engine new-package --licence MIT --name new-package"
+    );
+
+    // Wait for tool to complete
+    const statusCode = await runner.waitForStatusCode();
+
+    // Should exit in failure
+    expect(statusCode).toBe(1);
+
+    // Printed info to stderr
+    expect(runner.stderrLines).toContainInOrder([
+      "Must specify either --node-app or --library",
+    ]);
+
+    // Files not written to file system
+    expect(await fileSystem.fileExists("new-package/package.json")).toBe(false);
+    expect(await fileSystem.fileExists("new-package/src/main.ts")).toBe(false);
+  });
+
+  it("should enforce only one of --node-app or --library to be specified", async () => {
+    const runner = runCliCommand(
+      "yarn run ts-engine new-package --node-app --library --licence MIT --name new-package"
+    );
+
+    // Wait for tool to complete
+    const statusCode = await runner.waitForStatusCode();
+
+    // Should exit in failure
+    expect(statusCode).toBe(1);
+
+    // Printed info to stderr
+    expect(runner.stderrLines).toContainInOrder([
+      "Cannot specify both --node-app and --library, please provide one",
+    ]);
+
+    // Files not written to file system
+    expect(await fileSystem.fileExists("new-package/package.json")).toBe(false);
+    expect(await fileSystem.fileExists("new-package/src/main.ts")).toBe(false);
+  });
+
+  it("should enforce --name to be specified", async () => {
+    const runner = runCliCommand(
+      "yarn run ts-engine new-package --node-app --licence MIT"
+    );
+
+    // Wait for tool to complete
+    const statusCode = await runner.waitForStatusCode();
+
+    // Should exit in failure
+    expect(statusCode).toBe(1);
+
+    // Printed info to stderr
+    expect(runner.stderrLines).toContainInOrder([
+      "Missing required option --name",
+    ]);
+
+    // Files not written to file system
+    expect(await fileSystem.fileExists("new-package/package.json")).toBe(false);
+    expect(await fileSystem.fileExists("new-package/src/main.ts")).toBe(false);
+  });
+
+  it("should inform the user if the folder already exists and not create the package", async () => {
+    const runner = runCliCommand(
+      "yarn run ts-engine new-package --library --name existing-package"
+    );
+
+    // Wait for tool to complete
+    const statusCode = await runner.waitForStatusCode();
+
+    // Should exit successfully
+    expect(statusCode).toBe(1);
+
+    // Should inform the user
+    // Printed info to stderr
+    expect(runner.stderrLines).toContainInOrder([
+      "Folder existing-package already exists",
+    ]);
+
+    // Files not written to file system
+    expect(await fileSystem.fileExists("existing-package/package.json")).toBe(
+      false
+    );
+    expect(await fileSystem.fileExists("existing-package/src/main.ts")).toBe(
+      false
+    );
+  });
+});
---
+++ packages/cli/src/commands/new-package.ts
@@ -0,0 +1,148 @@
+import { spawnSync } from "child_process";
+import path from "path";
+import chalk from "chalk";
+import fs from "fs-extra";
+import { printError, printProgress } from "../utils/print";
+import type { Command, OutputType } from "../types";
+import {
+  createBooleanOption,
+  createStringOption,
+  argsToOptions,
+} from "../utils/options";
+
+const options = [
+  createBooleanOption({
+    name: "node-app",
+    description: "Outputs a Node.js application",
+    isRequired: false,
+    defaultValue: false,
+  }),
+  createBooleanOption({
+    name: "library",
+    description: "Outputs a JavaScript library",
+    isRequired: false,
+    defaultValue: false,
+  }),
+  createStringOption({
+    name: "license",
+    description: "The new package's license",
+    isRequired: false,
+    defaultValue: "UNLICENSED",
+  }),
+  createStringOption({
+    name: "name",
+    description: "The new package's name",
+    isRequired: true,
+    defaultValue: "",
+  }),
+];
+
+export interface NewPackageCommandOptions {
+  "node-app": boolean;
+  library: boolean;
+  license: string;
+  name: string;
+}
+
+export const newPackage: Command<NewPackageCommandOptions> = {
+  name: "new-package",
+  description: "Create a new package",
+  options,
+  run: async (args: string[]) => {
+    // Ensure envs are set
+    process.env.TS_ENGINE_COMMAND = "init";
+
+    const parsedOptions = argsToOptions<NewPackageCommandOptions>(
+      args,
+      options
+    );
+
+    // Required to specify whether building an Node.js app or a JavaScript library
+    if (!parsedOptions["node-app"] && !parsedOptions.library) {
+      const nodeAppOption = chalk.yellowBright("--node-app");
+      const libraryOption = chalk.yellowBright("--library");
+
+      printError(`Must specify either ${nodeAppOption} or ${libraryOption}`);
+      return Promise.reject();
+    }
+
+    if (parsedOptions["node-app"] && parsedOptions.library) {
+      const nodeAppOption = chalk.yellowBright("--node-app");
+      const libraryOption = chalk.yellowBright("--library");
+
+      printError(
+        `Cannot specify both ${nodeAppOption} and ${libraryOption}, please provide one`
+      );
+      return Promise.reject();
+    }
+
+    // TODO - provide npm package name validation and also only use
+    // the second part of a scoped name for the folder name
+    const newPackageDir = parsedOptions.name;
+
+    if (await fs.pathExists(path.resolve(process.cwd(), parsedOptions.name))) {
+      printError(`Folder ${newPackageDir} already exists`);
+      return Promise.reject();
+    }
+
+    // Determine output type
+    const outputType: OutputType = parsedOptions["node-app"]
+      ? "node-app"
+      : "library";
+
+    // Write files
+    const tsEngineCliVersion = spawnSync(
+      "npm",
+      ["show", "@ts-engine/cli", "version"],
+      { encoding: "utf8" }
+    ).stdout.replace("\n", "");
+    const writeFiles = async () => {
+      await fs.ensureDir(newPackageDir);
+      await fs.writeJSON(
+        path.resolve(newPackageDir, "package.json"),
+        {
+          name: parsedOptions.name,
+          version: "1.0.0",
+          license: parsedOptions.license,
+          private: outputType === "node-app",
+          scripts: {
+            build:
+              outputType === "node-app"
+                ? "ts-engine build --node-app"
+                : "ts-engine build --library",
+            lint: "ts-engine lint",
+            test: "ts-engine test",
+            typecheck:
+              outputType === "node-app"
+                ? "ts-engine typecheck"
+                : "ts-engine typecheck --emit",
+          },
+          devDependencies: {
+            "@ts-engine/cli": `^${tsEngineCliVersion}`,
+          },
+        },
+        { encoding: "utf8", spaces: 2 }
+      );
+      await fs.ensureFile(path.resolve(newPackageDir, "src/main.ts"));
+      await fs.writeFile(
+        path.resolve(newPackageDir, "src/main.ts"),
+        outputType === "node-app"
+          ? 'console.log("hello world");'
+          : 'export const printHelloWorld = () => console.log("hello world");',
+        { encoding: "utf8" }
+      );
+    };
+
+    await printProgress(
+      writeFiles(),
+      "Writing package files",
+      "new-package-write"
+    );
+
+    // Install ts-engine
+    spawnSync("yarn", {
+      cwd: newPackageDir,
+      stdio: "inherit",
+    });
+  },
+};
