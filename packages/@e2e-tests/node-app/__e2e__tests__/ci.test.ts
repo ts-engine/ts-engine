f013c0437755ccd0d8246b0b0eca925865fc00fb
leecheneler <lacheneler@gmail.com>
2020-05-14 11:47:50 +0100

    GH-29 - not using progress-estimator if in CI

---
+++ packages/@e2e-tests/node-app/__e2e__tests__/ci.test.ts
@@ -0,0 +1,27 @@
+import { fileSystem, runCliCommand } from "@helpers/test-utils";
+
+describe("ci", () => {
+  const originalCi = process.env.CI;
+
+  beforeEach(async () => {
+    await fileSystem.deleteDir("dist");
+    process.env.CI = "true";
+  });
+
+  afterEach(() => {
+    process.env.CI = originalCi;
+  });
+
+  it("should not use the progress estimator when in CI mode", async () => {
+    const runner = runCliCommand("yarn run ts-engine build --node-app");
+
+    // Wait for tool to complete
+    const statusCode = await runner.waitForStatusCode();
+
+    // Should exit successfully
+    expect(statusCode).toBe(0);
+
+    // Expect the build log to be outputted by itself, not part of the progress-estimator log
+    expect(runner.stdoutLines).toContainInOrder(["Building bundle"]);
+  });
+});
--- packages/cli/src/utils/print.ts
+++ packages/cli/src/utils/print.ts
@@ -15,10 +15,15 @@ export const printProgress = <TPromiseResult>(
   message: string,
   cacheName: string
 ): Promise<TPromiseResult> => {
-  const logEstimate = createLogger({
-    storagePath: path.join(__dirname, `.progress-estimator-${cacheName}`),
-  });
-  return logEstimate(promise, chalk.greenBright(message));
+  if (process.env.CI === "true") {
+    const logEstimate = createLogger({
+      storagePath: path.join(__dirname, `.progress-estimator-${cacheName}`),
+    });
+    return logEstimate(promise, chalk.greenBright(message));
+  }
+
+  console.log(chalk.greenBright(message));
+  return promise;
 };
 
 export const printSuccess = (message: string) => {
