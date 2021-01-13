7ac1c943b8e6abf71f402f7beabc787b31488eeb
leecheneler <lacheneler@gmail.com>
2021-01-13 21:29:53 +0000

    implemented test command

---
+++ packages/cli/src/constants.ts
@@ -0,0 +1,11 @@
+export const SUPPORTED_EXTENSIONS = [
+  "js",
+  "ts",
+  "jsx",
+  "tsx",
+  "mjs",
+  "cjs",
+  "es",
+];
+
+export const SUPPORTED_EXTENSIONS_REGEX = `(${SUPPORTED_EXTENSIONS.join("|")})`;
