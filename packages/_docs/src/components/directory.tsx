424a337aeab493cc9d5e8a56fc904a0029840b86
leecheneler <lacheneler@gmail.com>
2021-01-20 22:28:02 +0000

    desktop layout, start of mobile layout for docs

--- packages/_docs/src/components/directory.tsx
+++ packages/_docs/src/components/directory.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useRouter } from "next/router";
+import Link from "next/link";
 import { Doc } from "../lib/docs";
 
 export interface DirectoryProps {
@@ -9,20 +10,27 @@ export interface DirectoryProps {
 export const Directory = (props: DirectoryProps) => {
   const router = useRouter();
 
-  const pathMatches = (slug: string) => {
-    return router.pathname === `/docs/${slug}`;
+  const pathMatches = (path: string) => {
+    return router.asPath === path;
   };
 
   return (
     <nav>
       <ul>
         {props.docs.map((p) => {
-          const activeClass = pathMatches(p.slug) ? "active" : "";
+          const path = `/docs/${p.slug}`;
+          const activeClass = pathMatches(path)
+            ? "text-blue-500 underline"
+            : "";
           return (
-            <li key={p.slug}>
-              <a href={`/docs/${p.slug}`} className={`${activeClass}`}>
-                {p.title}
-              </a>
+            <li key={p.slug} className="pb-2">
+              <Link href={`/docs/${p.slug}`}>
+                <a
+                  className={`block w-100 hover:underline focus:underline whitespace-nowrap ${activeClass}`}
+                >
+                  {p.title}
+                </a>
+              </Link>
             </li>
           );
         })}
--- packages/_docs/src/components/layout.tsx
+++ packages/_docs/src/components/layout.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Doc } from "../lib/docs";
 import { Directory } from "./directory";
+import TsEngineSvg from "../icons/ts-engine.svg";
+import { version } from "../../package.json";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -10,11 +12,23 @@ interface LayoutProps {
 export const Layout = (props: LayoutProps) => {
   return (
     <div>
-      <header>ts-engine</header>
-      <aside>
-        <Directory docs={props.docs} />
-      </aside>
-      <main>{props.children}</main>
+      <header className="font-semibold font-mono p-2 md:p-4 bg-blue-500 text-white">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center">
+            <div className="pr-4">
+              <TsEngineSvg className="w-16 h-16 md:w-20 md:h-20" />
+            </div>
+            <div className="text-xl md:text-3xl">ts-engine</div>
+          </div>
+          <div className="text-sm md:text-lg">{version}</div>
+        </div>
+      </header>
+      <div className="flex flex-col md:flex-row">
+        <aside className="pb-4 pl-0 md:p-6">
+          <Directory docs={props.docs} />
+        </aside>
+        <main className="container mx-auto p-6">{props.children}</main>
+      </div>
     </div>
   );
 };
