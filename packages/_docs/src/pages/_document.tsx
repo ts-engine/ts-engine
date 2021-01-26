d51e1318c66de3848606ce3b7f3b5dd8f8c31f9d
Lee Cheneler <lacheneler@gmail.com>
2021-01-26 23:15:43 +0000

    lighthouse improvements (#141)
    * lighthouse improvements
    
    * fixed linting

---
+++ packages/_docs/src/pages/_document.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+
+class Document extends NextDocument {
+  static async getInitialProps(ctx) {
+    const initialProps = await NextDocument.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta
+            name="Description"
+            content="Write TypeScript packages with optionally zero configuration."
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default Document;
