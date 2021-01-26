a40dcb316e224f57fcca0daef3ad6a8cacf54d35
leecheneler <lacheneler@gmail.com>
2021-01-26 22:53:25 +0000

    added aws-lambda example

---
+++ packages/_examples/aws-lambda/src/index.ts
@@ -0,0 +1,3 @@
+export const handler = async () => {
+  console.log("Hell from your lambda!");
+};
--- packages/_examples/express/src/index.ts
+++ packages/_examples/express/src/index.ts
@@ -1,19 +1,9 @@
 import express from "express";
-import { add, subtract } from "@examples/maths";
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
-});
-
-app.get("/maths/add/:a/:b", (req, res) => {
-  console.log(req.params);
-  res.send(add(+req.params.a, +req.params.b).toString());
-});
-
-app.get("/maths/subtract/:a/:b", (req, res) => {
-  res.send(subtract(+req.params.a, +req.params.b).toString());
 });
 
 app.listen(3000, () => {
